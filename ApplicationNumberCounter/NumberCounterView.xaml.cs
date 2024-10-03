10c47e86cd14bd74163ad92d646a0b95adb3509a
Vinicius Santos <vdosantos12@hotmail.com>
2024-10-02 22:12:19 -0300

    create numeric counter

---
+++ ApplicationNumberCounter/NumberCounterView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace ApplicationNumberCounter
+{
+    /// <summary>
+    /// Interaction logic for NumberCounterView.xaml
+    /// </summary>
+    public partial class NumberCounterView : Window
+    {
+
+        private int _counter;
+        public NumberCounterView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Decrease_Click(object sender, RoutedEventArgs e)
+        {
+            _counter--;
+            UpdateCounter();
+        }
+
+        private void Button_Increase_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            UpdateCounter();
+        }
+
+        private void Button_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            _counter = 0;
+            UpdateCounter();
+        }
+
+        private void UpdateCounter()
+        {
+            txtCounter.Text = _counter.ToString();
+        }
+    }
+}
